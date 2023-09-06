8d42c30bd13e4d0920a6fb1cc3d1c07097f7ccd9
teresa881016 <teresa881016@gmail.com>
2023-09-06 23:48:59 +0900

    @ManyToMany 단방향 테스트

--- src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -18,7 +18,14 @@ public class Food {
     private String name;
     private double price;
 
-    @OneToMany
-    @JoinColumn(name = "food_id") // users 테이블에 food_id 컬럼
+    @ManyToMany
+    @JoinTable(name = "orders", // 중간 테이블 생성
+            joinColumns = @JoinColumn(name = "food_id"), // 현재 위치인 Food Entity 에서 중간 테이블로 조인할 컬럼 설정
+            inverseJoinColumns = @JoinColumn(name = "user_id")) // 반대 위치인 User Entity 에서 중간 테이블로 조인할 컬럼 설정
     private List<User> userList = new ArrayList<>();
+
+    public void addUserList(User user) {
+        this.userList.add(user); // 외래 키(연관 관계) 설정
+        user.getFoodList().add(this);
+    }
 }
